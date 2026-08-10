ed05ad536345380747865b8d0e9b973effbba5a1
hatumhussain <hatumhussain@gmail.com>
2026-08-10 18:45:23 +0500

    Add python learning program

---
+++ variables and datatypes.py
@@ -0,0 +1,7 @@
+# Create a Simple Calculator 
+a = 67
+b = 78
+print("addition of a and b is :" ,  a + b)
+print("Substraction of a and b :", a - b)
+print("Multiplication of a and b :" , a*b)
+print("Division of a and b: ", a/b)
