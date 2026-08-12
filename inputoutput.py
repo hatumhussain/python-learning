a281f95ce2e4133db8d96d7eda3cc296be5479ca
hatumhussain <hatumhussain@gmail.com>
2026-08-12 16:17:16 +0500

    How to take input and output using python programming

---
+++ inputoutput.py
@@ -0,0 +1,9 @@
+# Input in Python
+print("Welcome to the Arithmetic Calculator")
+a = input("Enter the First Number: "  )
+b = input("Enter the Second Number: " )
+
+print("The Value for Addition of two Numbers is: " ,  int(a) + int(b))
+print("The Value for Substraction of two Numbers is: " ,  int(a) - int(b))
+print("The Value for Multiplication of two Numbers is: " ,  int(a) * int(b))
+print("The Value for Division  of two Numbers is: " ,  int(a) / int(b))
