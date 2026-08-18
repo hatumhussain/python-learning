4811893b0531c707cd8c3066edd0809b400da908
hatumhussain <hatumhussain@gmail.com>
2026-08-18 13:32:42 +0500

    Learning Strings

---
+++ StringsDatatypes.py
@@ -0,0 +1,10 @@
+#Strings Datatypes
+Name = "Hatum Hussain Mirjat"
+print("Hellow " , Name)
+
+
+#multiple line strings- We have write the multiple line codes with open ''' close with ''''
+Coder = '''Hallow 
+I am learning python 
+For playwright automation'''
+print(Coder)
