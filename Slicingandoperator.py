8546c5ae205695c884d7ed7cb67b5ea930e6343f
Hatum Hussain <61963956+hatumhussain@users.noreply.github.com>
2026-08-20 01:28:25 +0500

    How to Add the slicing in string

---
+++ Slicingandoperator.py
@@ -0,0 +1,9 @@
+#String Slicing and operation on strings
+names = "Hatum,Mirjat"
+#for finding the length of this string 
+print(len(names))
+fruit = "mango"
+print(len(fruit))
+print(fruit[:])
+#Nagative Slicing 
+print(fruit[0:-3])
