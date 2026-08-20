90f5c9516bc1643750d70129c302d54ec40d73c0
Hatum Hussain <61963956+hatumhussain@users.noreply.github.com>
2026-08-20 15:39:37 +0500

    How to use Strings Methods using python

---
+++ StringMethods.py
@@ -0,0 +1,13 @@
+#Strings Methods in Python 
+#How to use string in uppar case/lower case and find the length 
+#Strings are immetuable strings means not changed 
+Name = "Hatum!!!!!!!!!"
+print(len(Name))
+print(Name.upper()) #Display the uppar case characters 
+print(Name.lower()) #Display the lower case characters
+print(Name.rstrip("!")) #it elminates the !!!
+print(Name.replace("Hatum","Mirjat")) #it will replace the strings
+blogheading = "introduction TO learning Python lAguage "
+print(blogheading.capitalize()) #It capitilized to the first letter
+Str1 = "He is a software tester"
+print(Str1.find("software")) #It returns the index of finding string and if string is not find then it return -1 value
